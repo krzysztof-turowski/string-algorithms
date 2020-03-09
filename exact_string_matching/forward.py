84bc008a0d3764c1f32e9ec7ca302ebc2346e142
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-03-09 20:52:33 +0100

    Fix KMP-based algorithms and change to generators

--- exact_string_matching/forward.py
+++ exact_string_matching/forward.py
@@ -1,34 +1,31 @@
 from common import prefix
 
 def brute_force(t, w, n, m):
-  for i in range(n - m + 1):
+  for i in range(1, n - m + 2):
     j = 0
-    while j < m and t[i + j + 1] == w[j + 1]:
+    while j < m and t[i + j] == w[j + 1]:
       j = j + 1
     if j == m:
-      return True
-  return False
+      yield i
 
 def morris_pratt(t, w, n, m):
   B = prefix.prefix_suffix(w, m)
-  i = 0
+  i, j = 1, 0
   while i <= n - m + 1:
-    j = 0
-    while j < m and t[i + j + 1] == w[j + 1]:
+    while j < m and t[i + j] == w[j + 1]:
       j = j + 1
     if j == m:
-      return True
-    i, j = i + j - B[j], max(0, B[j])
-  return False
+      yield i
+      i, j = i + j - B[j], max(0, B[j])
+    else:
+      i, j = i + j - B[j], max(0, B[j])
 
 def knuth_morris_pratt(t, w, n, m):
   sB = prefix.strong_prefix_suffix(w, m)
-  i = 0
+  i, j = 1, 0
   while i <= n - m + 1:
-    j = 0
-    while j < m and t[i + j + 1] == w[j + 1]:
-      j = j + 1
+    while j < m and t[i + j] == w[j + 1]:
+      j += 1
     if j == m:
-      return True
+      yield i
     i, j = i + j - sB[j], max(0, sB[j])
-  return False
