71aec0075344bc0fde9fc00d041f3389098540ba
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-03-09 19:06:58 +0100

    Add random word generator

---
+++ generator/random.py
@@ -0,0 +1,5 @@
+import random
+
+def random_word(m, A, p = None):
+  '''Generates random m-letter word (1-based) with letters in A and probabilities p'''
+  return '#' + ''.join(letter for letter in random.choices(A, p, k = m))
