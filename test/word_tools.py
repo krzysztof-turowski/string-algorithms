b97f57aff7bba8e3813b912517f7d6f4c3ff2cd3
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-03-10 02:28:11 +0100

    Add debug mode to word_tools

--- test/word_tools.py
+++ test/word_tools.py
@@ -1,9 +1,11 @@
 class ComparisonCount:
-  def __init__(self, w):
-    self.w, self.compared = w, 0
+  def __init__(self, w, debug = False):
+    self.w, self.compared, self.debug = w, 0, debug
 
   def __getitem__(self, key):
     self.compared += 1
+    if self.debug:
+      print(key, self.w[key])
     return self.w[key]
 
   def __len__(self):
