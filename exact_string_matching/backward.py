5c3d0b72366dc7db6375db407f5e4f98f94c309e
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-03-10 12:39:11 +0100

    Add Galil rule

--- exact_string_matching/backward.py
+++ exact_string_matching/backward.py
@@ -32,6 +32,19 @@ def boyer_moore(t, w, n, m):
       yield i
     i = i + BM[j]
 
+def boyer_moore_galil(t, w, n, m):
+  BM = suffix.boyer_moore_shift(w, m)
+  i, memory = 1, 0
+  while i <= n - m + 1:
+    j = m
+    while j > memory and t[i + j - 1] == w[j]:
+      j = j - 1
+    if j == memory:
+      yield i
+      i, memory = i + BM[0], m - BM[0]
+    else:
+      i, memory = i + BM[j], 0
+
 def boyer_moore_bad_shift(t, w, n, m):
   BM, LAST = suffix.boyer_moore_shift(w, m), suffix.last_occurrence(w)
   i = 1
