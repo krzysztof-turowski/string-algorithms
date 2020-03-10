5ada3c6c285bb411b4a8d53953fa7b6e88825d4b
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-03-10 03:16:53 +0100

    Add bad shift heuristic to Boyer-Moore

--- common/suffix.py
+++ common/suffix.py
@@ -1,7 +1,8 @@
 from common import prefix, varia
 
 def weak_boyer_moore_shift_brute_force(w, m):
-  wBM = [m] * (m + 1)
+  B = prefix.prefix_suffix(w, m)
+  wBM = [m] * m + [B[m]]
   for i in range(1, m + 1):
     for s in range(1, m + 1):
       start = max(i, s) + 1
@@ -16,9 +17,10 @@ def weak_boyer_moore_shift(w, m):
   return weak_boyer_moore_shift_brute_force(w, m)
 
 def boyer_moore_shift_brute_force(w, m):
-  BM = [m] * (m + 1)
-  for i in range(1, m + 1):
-    for s in range(1, m + 1):
+  B = prefix.prefix_suffix(w, m)
+  BM = [m] * m + [B[m]]
+  for i in range(m):
+    for s in range(1, m):
       start = max(i, s) + 1
       if w[start - s:-s] == w[start:] and (s >= i or w[i - s] != w[i]):
         BM[i] = s
@@ -32,9 +34,10 @@ def maximum_suffixes(w, m):
   return S
 
 def boyer_moore_shift(w, m):
-  BM = [m] * (m + 1)
+  B = prefix.prefix_suffix(w, m)
+  BM = [m] * m + [B[m]]
   S, j = maximum_suffixes(w, m), 0
-  for k in range(m, -1, -1):
+  for k in range(m - 1, -1, -1):
     if k == S[k]:
       while j < m - k:
         BM[j] = m - k
@@ -42,3 +45,9 @@ def boyer_moore_shift(w, m):
   for k in range(1, m):
     BM[m - S[k]] = m - k
   return BM
+
+def last_occurrence(w, m):
+  LAST = {}
+  for i, v in enumerate(w[1:]):
+    LAST[v] = i + 1
+  return LAST
--- exact_string_matching/backward.py
+++ exact_string_matching/backward.py
@@ -31,3 +31,27 @@ def boyer_moore(t, w, n, m):
     if j == 0:
       yield i
     i = i + BM[j]
+
+def boyer_moore_bad_shift(t, w, n, m):
+  BM, LAST = suffix.boyer_moore_shift(w, m), suffix.last_occurrence(w, m)
+  i = 1
+  while i <= n - m + 1:
+    j = m
+    while j > 0 and t[i + j - 1] == w[j]:
+      j = j - 1
+    if j == 0:
+      yield i
+    bad_character = LAST.get(t[i + j - 1], 0) if i + j <= n else 0
+    i = i + max(BM[j], j - bad_character)
+
+def bad_shift_heuristic(t, w, n, m):
+  LAST = suffix.last_occurrence(w, m)
+  i = 1
+  while i <= n - m + 1:
+    j = m
+    while j > 0 and t[i + j - 1] == w[j]:
+      j = j - 1
+    if j == 0:
+      yield i
+    bad_character = LAST.get(t[i + j - 1], 0) if i + j <= n else 0
+    i = i + max(1, j - bad_character)
