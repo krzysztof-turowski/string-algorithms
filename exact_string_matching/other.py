be02eeb3d8921d9731b32d0edb7fc6c64539e8fd
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-04-01 14:55:55 +0200

    Fix name

--- exact_string_matching/other.py
+++ exact_string_matching/other.py
@@ -4,10 +4,10 @@ from exact_string_matching import forward
 from string_indexing import suffix_tree
 
 def fast_on_average(t, w, n, m):
-  S, _ = suffix_tree.mccreight(w, m)
+  ST, _ = suffix_tree.mccreight(w, m)
   i, r = m, min(2 * math.ceil(math.log(m, 2)), m - 1)
   while i <= n:
-    if S.contains(t[(i - r):(i + 1)]):
+    if ST.contains(t[(i - r):(i + 1)]):
       subt = t[0] + t[(i - m + 1):min(i + m - r, n + 1)]
       subn = min(i + m - r, n + 1) - (i - m + 1)
       yield from [v + i - m for v in forward.knuth_morris_pratt(subt, w, subn, m)]
