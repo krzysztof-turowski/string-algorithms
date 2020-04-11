d9792577ef26e013c9cdab57118bd4f1bfac8020
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-04-11 02:38:18 +0200

    Fix style and naming issues

--- string_indexing/suffix_array.py
+++ string_indexing/suffix_array.py
@@ -1,8 +1,6 @@
-import collections
-
 def _reverse(S):
   reverse = [0] * len(S)
-  for i in range(len(S)):
+  for i, _ in enumerate(S):
     reverse[S[i] - 1] = i + 1
   return reverse
 
@@ -12,7 +10,7 @@ def _rank(S):
 
 def _merge(A, B, compare):
   out, a, b = [0] * (len(A) + len(B)), 0, 0
-  for i in range(len(out)):
+  for i, _ in enumerate(out):
     if b >= len(B):
       out[i], a = A[a], a + 1
     elif a >= len(A) or compare(A[a], B[b]):
@@ -41,26 +39,25 @@ def skew(text, n):
     return '#' + ''.join(chr(ord('0') + v) for v in data)
   def compare(i, j):
     if i % 3 == 1:
-      return (text[i], S.get(i + 1, -1)) >= (text[j], S.get(j + 1, -1))
-    else:
-      return ((text[i:i + 2], S.get(i + 2, -1))
-          >= (text[j:j + 2], S.get(j + 2, -1)))
+      return (text[i], S.get(i + 1, 0)) >= (text[j], S.get(j + 1, 0))
+    return (text[i:i + 2], S.get(i + 2, 0)) >= (text[j:j + 2], S.get(j + 2, 0))
 
   if n <= 4:
     return naive(text, n)
   text += '$'
-  P01 = [i for i in range(1, n + 2) if i % 3 == 1] \
+  P12 = [i for i in range(1, n + 2) if i % 3 == 1] \
       + [i for i in range(1, n + 2) if i % 3 == 2]
-  triples = _rank([text[i:i + 3] for i in P01])
+  triples = _rank([text[i:i + 3] for i in P12])
   recursion = skew(convert(triples), (2 * n + 1) // 3 + 1)[1:]
-  L01 = [P01[v - 1] for v in recursion]
-  mapping = {v: i + 1 for i, v in enumerate(L01)}
-  S = {v: mapping[v] for v in P01}
-  
-  P2 = [i for i in range(1, n + 2) if i % 3 == 0]
-  tuples = [(text[i], S.get(i + 1, -1)) for i in P2]
-  L2 = [P2[i - 1] for i in _reverse(_rank(tuples))]
-  return _merge(L01, L2, compare = compare)
+  L12 = [P12[v - 1] for v in recursion]
+
+  mapping = {v: i + 1 for i, v in enumerate(L12)}
+  S = {v: mapping[v] for v in P12}
+
+  P0 = [i for i in range(1, n + 2) if i % 3 == 0]
+  tuples = [(text[i], S.get(i + 1, 0)) for i in P0]
+  L0 = [P0[i - 1] for i in _reverse(_rank(tuples))]
+  return _merge(L12, L0, compare = compare)
 
 def induced_sorting(text, n):
   '''Computes suffix array using Nong-Zhang-Chan algorithm'''
