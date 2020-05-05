c25eb8d1060f74c656bfebc193f1106ececbebc6
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-05 23:56:12 +0200

    Add critical factorization algorithms

---
+++ lyndon/critical_factorization.py
@@ -0,0 +1,29 @@
+import operator
+
+from common import prefix
+from lyndon import maximum_suffix
+
+def local_period_naive(text_left, text_right):
+  for i in range(1, len(text_right) + 1):
+    w = text_right[:i]
+    if text_left.endswith(w) or w.endswith(text_left):
+      return w
+  for i in range(1, len(text_left) + 1):
+    w = text_right + text_left[-i:]
+    if text_left.endswith(w) or w.endswith(text_left):
+      return w
+
+def naive_all(text, n):
+  p = prefix.period(text, n)
+  out = []
+  for i in range(1, n + 2):
+    w = local_period_naive(text[1:i], text[i:])
+    if len(w) == p:
+      out.append((i, prefix.period(text[0] + text[i:], n + 1 - i)))
+  return out
+
+def constant_space(text, n):
+  index_lt, p_lt = maximum_suffix.constant_space(text, n)
+  index_gt, p_gt = maximum_suffix.constant_space(
+      text, n, less = operator.__gt__)
+  return (index_lt, p_lt) if index_lt >= index_gt else (index_gt, p_gt)
---
+++ test/test_critical_factorization.py
@@ -0,0 +1,44 @@
+import itertools
+import os
+import unittest
+
+from generator import rand
+from lyndon import critical_factorization
+
+CRITICAL_FACTORIZATION_ALGORITHMS = [
+    critical_factorization.constant_space
+]
+
+class TestCriticalFactorization(unittest.TestCase):
+  run_large = unittest.skipUnless(
+      os.environ.get('LARGE', False), 'Skip test in small runs')
+
+  def check_critical_factorization(self, t, n, reference):
+    for algorithm in CRITICAL_FACTORIZATION_ALGORITHMS:
+      self.assertIn(
+          algorithm(t, n),
+          reference,
+          'Algorithm {0} on text {1}'.format(algorithm.__name__, t))
+
+  def test_critical_factorization(self):
+    self.check_critical_factorization('#aababb', 6, [(5, 1)])
+    self.check_critical_factorization('#GCAGAGAG', 8, [(3, 2)])
+    self.check_critical_factorization('#ababbaba', 8, [(4, 5)])
+    self.check_critical_factorization('#abaabaa', 7, [(3, 3)])
+
+  @run_large
+  def test_random_critical_factorization(self):
+    T, n, A = 100, 300, ['a', 'b']
+    for _ in range(T):
+      t = rand.random_word(n, A)
+      reference = critical_factorization.naive_all(t, n)
+      self.check_critical_factorization(t, n, reference)
+
+  @run_large
+  def test_all_critical_factorization(self):
+    N, A = 14, ['a', 'b']
+    for n in range(2, N + 1):
+      for t in itertools.product(A, repeat = n):
+        t = '#' + ''.join(t)
+        reference = critical_factorization.naive_all(t, n)
+        self.check_critical_factorization(t, n, reference)
