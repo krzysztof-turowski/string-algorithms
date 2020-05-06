4f947b35771a0955eb90fcd71b88ce616211e137
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-07 01:53:45 +0200

    Implement basic FFT string matching with don't cares

--- approximate_string_matching/dont_care.py
+++ approximate_string_matching/dont_care.py
@@ -1,5 +1,18 @@
-def brute_force(text, word, n, m):
-  raise NotImplementedError
+import scipy.signal
 
 def basic_fft(text, word, n, m):
-  raise NotImplementedError
+  if n < m:
+    return
+  A = set(list(text[1:] + word[1:]))
+  A.discard('?')
+  mismatches = [0] * (n - m + 1)
+  for a in A:
+    text_binary = [int(c == a) for c in text[1:]]
+    for b in A:
+      if a == b:
+        continue
+      word_binary = [int(c == b) for c in reversed(word[1:])]
+      mismatches_ab = scipy.signal.convolve(
+        text_binary, word_binary, mode = 'valid', method = 'fft')
+      mismatches = [x + y for x, y in zip(mismatches, mismatches_ab)]
+  yield from (i + 1 for i, v in enumerate(mismatches) if v == 0)
