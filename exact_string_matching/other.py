d8745657990b85fe5325a301fee4d456369fbf6e
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-06 06:50:13 +0200

    Add Karp-Rabin algorithm

--- exact_string_matching/other.py
+++ exact_string_matching/other.py
@@ -1,8 +1,22 @@
 import math
+import random
 
 from exact_string_matching import forward
 from lyndon import critical_factorization
 from string_indexing import suffix_tree
+
+def karp_rabin(text, word, n, m):
+  MOD = 257
+  A = random.randint(2, MOD - 1)
+  Am = pow(A, m, MOD)
+  def generate(t):
+    return sum(ord(c) * pow(A, i, MOD) for i, c in enumerate(t[::-1])) % MOD
+  text += '$'
+  hash_text, hash_word = generate(text[1:m + 1]), generate(word[1:])
+  for i in range(1, n - m + 2):
+    if hash_text == hash_word and text[i:i + m] == word[1:]:
+      yield i
+    hash_text = (A * hash_text + ord(text[i + m]) - Am * ord(text[i])) % MOD
 
 def fast_on_average(text, word, n, m):
   ST, _ = suffix_tree.mccreight(word, m)
