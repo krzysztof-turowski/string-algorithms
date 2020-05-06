424a96155594d626b59491d2dd30fb9d02a223d9
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-06 18:25:36 +0200

    Use namedtuple in LCS auxiliary structure

--- approximate_string_matching/lcs.py
+++ approximate_string_matching/lcs.py
@@ -1,26 +1,29 @@
+from collections import namedtuple
+
 def needleman_wunsch(text_1, text_2, n_1, n_2):
-  S = { }
-  for i in range(n_1 + 1):
-    S[(i, 0)] = (i, (0, 0))
-  for i in range(1, n_2 + 1):
-    S[(0, i)] = (i, (0, 0))
+  Data = namedtuple('Data', ['distance', 'previous', 'letter'])
+  d = { (0, 0): Data(0, None, '') }
+  for i, ci in enumerate(text_1[1:]):
+    d[(i + 1, 0)] = Data(i + 1, (i, 0), ci)
+  for i, ci in enumerate(text_2[1:]):
+    d[(0, i + 1)] = Data(i + 1, (0, i), ci)
   for i, ci in enumerate(text_1[1:]):
     for j, cj in enumerate(text_2[1:]):
-      insertion = (S[(i, j + 1)][0] + 1, (i, j + 1))
-      deletion = (S[(i + 1, j)][0] + 1, (i + 1, j))
+      insertion = Data(d[(i, j + 1)].distance + 1, (i, j + 1), '')
+      deletion = Data(d[(i + 1, j)].distance + 1, (i + 1, j), '')
       if ci == cj:
-        agreement = (S[(i, j)][0], (i, j), ci)
-        if agreement[0] <= min(insertion[0], deletion[0]):
-          S[(i + 1, j + 1)] = agreement
+        agreement = Data(d[(i, j)].distance, (i, j), ci)
+        if agreement.distance <= min(insertion.distance, deletion.distance):
+          d[(i + 1, j + 1)] = agreement
           continue
-      if insertion[0] <= deletion[0]:
-        S[(i + 1, j + 1)] = insertion
+      if insertion.distance <= deletion.distance:
+        d[(i + 1, j + 1)] = insertion
       else:
-        S[(i + 1, j + 1)] = deletion
+        d[(i + 1, j + 1)] = deletion
   text, p = '', (n_1, n_2)
   while p != (0, 0):
-    p_next = S[p][1]
+    p_next = d[p].previous
     if p[0] - p_next[0] == 1 and p[1] - p_next[1] == 1:
-      text = S[p][2] + text
+      text = d[p].letter + text
     p = p_next
-  return text, S[(n_1, n_2)][0]
+  return text, d[(n_1, n_2)].distance
