6a4891845b05979a51af3e06254171a73aadf39d
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-07 17:26:49 +0200

    Approximate string matching and LCS algorithms (#3)
    * Add Hamming and edit distance
    * Add Needleman-Wunsch and Hirschberg LCS algorithm
    * Implement basic FFT string matching with don't cares

---
+++ approximate_string_matching/distance.py
@@ -0,0 +1,41 @@
+from collections import namedtuple
+import math
+
+ScoreMatrix = namedtuple(
+    'ScoreMatrix', ['insert', 'delete', 'substitute', 'match'])
+
+def hamming_distance(text_1, text_2, n_1, n_2):
+  if n_1 != n_2:
+    raise ValueError('Hamming distance is defined only for equal strings')
+  return sum(ci != cj for ci, cj in zip(text_1[1:], text_2[1:]))
+
+def distance_row(text_1, text_2, _, n_2, S):
+  previous_row, current_row = None, range(n_2 + 1)
+  for i, ci in enumerate(text_1[1:]):
+    previous_row, current_row = current_row, [i + 1] + [None] * n_2
+    for j, cj in enumerate(text_2[1:]):
+      insertion = previous_row[j + 1] + S.insert(ci)
+      deletion = current_row[j] + S.delete(cj)
+      if ci != cj:
+        substitution = previous_row[j] + S.substitute(ci, cj)
+      else:
+        substitution = previous_row[j] + S.match(ci)
+      current_row[j + 1] = min(insertion, deletion, substitution)
+  return current_row
+
+def edit_distance(text_1, text_2, n_1, n_2):
+  S = ScoreMatrix(
+      insert = lambda ci: 1, delete = lambda ci: 1,
+      substitute = lambda ci, cj: 1, match = lambda ci: 0)
+  if n_1 >= n_2:
+    return distance_row(text_1, text_2, n_1, n_2, S)[-1]
+  return distance_row(text_2, text_1, n_2, n_1, S)[-1]
+
+def indel_distance_row(text_1, text_2, n_1, n_2):
+  S = ScoreMatrix(
+      insert = lambda ci: 1, delete = lambda ci: 1,
+      substitute = lambda ci, cj: math.inf, match = lambda ci: 0)
+  return distance_row(text_1, text_2, n_1, n_2, S)
+
+def indel_distance(text_1, text_2, n_1, n_2):
+  return indel_distance_row(text_1, text_2, n_1, n_2)[-1]
---
+++ test/test_approximate_string_matching.py
@@ -0,0 +1,71 @@
+import itertools
+import os
+import unittest
+
+import regex
+
+from approximate_string_matching import dont_care
+from generator import rand
+
+STRING_MATCHING_WITH_DONT_CARE_ALGORITHMS = [
+    dont_care.basic_fft,
+]
+
+class TestStringMatchingWithDontCare(unittest.TestCase):
+  run_large = unittest.skipUnless(
+      os.environ.get('LARGE', False), 'Skip test in small runs')
+
+  def check_get_first_match_with_dont_care(self, t, w, n, m, reference):
+    for algorithm in STRING_MATCHING_WITH_DONT_CARE_ALGORITHMS:
+      self.assertEqual(
+          next(algorithm(t, w, n, m)),
+          reference,
+          'Algorithm {}, text {}, pattern {}'.format(
+              algorithm.__name__, t, w))
+
+  def check_get_all_matches_with_dont_care(self, t, w, n, m, reference):
+    for algorithm in STRING_MATCHING_WITH_DONT_CARE_ALGORITHMS:
+      self.assertEqual(
+          list(algorithm(t, w, n, m)),
+          reference,
+          'Algorithm {}, text {}, pattern {}'.format(
+              algorithm.__name__, t, w))
+
+  def check_no_match(self, t, w, n, m):
+    for algorithm in STRING_MATCHING_WITH_DONT_CARE_ALGORITHMS:
+      self.assertFalse(
+          list(algorithm(t, w, n, m)),
+          'Algorithm {}, text {}, pattern {}'.format(
+              algorithm.__name__, t, w))
+
+  def test_get_first_match_with_dont_care(self):
+    self.check_get_first_match_with_dont_care('#abbaba', '#?ba', 6, 3, 2)
+
+  def test_get_all_matches_with_dont_care(self):
+    self.check_get_all_matches_with_dont_care(
+        '#abaaabbaababb', '#a?b', 13, 3, [4, 5, 8, 11])
+
+  def test_no_match(self):
+    self.check_no_match('#abaaba', '#b???b', 6, 5)
+
+  @run_large
+  def test_random_string_matching_with_dont_care(self):
+    T, n, m, A = 100, 500, 10, ['a', 'b']
+    for _ in range(T):
+      t, w = rand.random_word(n, A), rand.random_word(m, A)
+      reference = [match.start() + 1 for match in regex.finditer(
+          w[1:].replace('?', '.'), t[1:], overlapped = True)]
+      self.check_get_all_matches_with_dont_care(t, w, n, m, reference)
+
+  @run_large
+  def test_all_string_matching_with_dont_care(self):
+    N, M, A = 7, 3, ['a', 'b']
+    for n in range(2, N + 1):
+      for t in itertools.product(A, repeat = n):
+        t = '#' + ''.join(t)
+        for m in range(1, M + 1):
+          for w in itertools.product(A + ['?'], repeat = m):
+            w = '#' + ''.join(w)
+            reference = [match.start() + 1 for match in regex.finditer(
+                w[1:].replace('?', '.'), t[1:], overlapped = True)]
+            self.check_get_all_matches_with_dont_care(t, w, n, m, reference)
