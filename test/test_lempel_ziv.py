fe48f19605d1e9c336ae18396248738ee1752646
krzysztof-turowski <krzysztof.szymon.turowski@gmail.com>
2020-05-10 16:38:16 +0200

    Implement simple LZ77 and its inverse

--- test/test_lempel_ziv.py
+++ test/test_lempel_ziv.py
@@ -3,6 +3,7 @@ import os
 import unittest
 
 from compression import lempel_ziv
+from generator import rand
 
 class TestLempelZiv77(unittest.TestCase):
   run_large = unittest.skipUnless(
@@ -11,6 +12,9 @@ class TestLempelZiv77(unittest.TestCase):
   def check_lempel_ziv_77(self, t, n, reference):
     self.assertEqual(lempel_ziv.lz77(t, n), reference)
 
+  def check_recode_lempel_ziv_77(self, t, n):
+    self.assertEqual(lempel_ziv.inverse_lz77(lempel_ziv.lz77(t, n)), t)
+
   def test_lempel_ziv_77(self):
     self.check_lempel_ziv_77(
         '#ababcbababaa', 12,
@@ -18,3 +22,22 @@ class TestLempelZiv77(unittest.TestCase):
     self.check_lempel_ziv_77(
         '#aacaacabcabaaac', 15,
         [(0, 0, 'a'), (1, 1, 'c'), (3, 4, 'b'), (3, 3, 'a'), (1, 2, 'c')])
+
+  def test_recode_lempel_ziv_77(self):
+    self.check_recode_lempel_ziv_77('#ababcbababaa', 12)
+    self.check_recode_lempel_ziv_77('#aacaacabcabaaac', 15)
+
+  @run_large
+  def test_random_recode_lempel_ziv_77(self):
+    T, n, A = 100, 500, ['a', 'b']
+    for _ in range(T):
+      t = rand.random_word(n, A)
+      self.check_recode_lempel_ziv_77(t, n)
+
+  @run_large
+  def test_all_recode_lempel_ziv_77(self):
+    N, A = 12, ['a', 'b']
+    for n in range(2, N + 1):
+      for t in itertools.product(A, repeat = n):
+        t = '#' + ''.join(t)
+        self.check_recode_lempel_ziv_77(t, n)
