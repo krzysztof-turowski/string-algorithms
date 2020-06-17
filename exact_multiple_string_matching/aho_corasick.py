d1d72e53983040629658dfab69340f2c1c62077c
Piotr Mikolajczyk <piomiko41@gmail.com>
2020-06-17 19:47:54 +0200

    Do not store keywords; Rename next -> nxt

--- exact_multiple_string_matching/aho_corasick.py
+++ exact_multiple_string_matching/aho_corasick.py
@@ -9,12 +9,13 @@ def find_occurrences(text, n, ac_automaton):
   return ac_automaton.find_occurrences(text, n)
 
 
+# pylint: disable=too-few-public-methods
 class AhoCorasickAutomaton:
   def __init__(self, keywords, alphabet):
     self._root = AhoCorasickAutomaton.Node()
     self._construct_goto(keywords, alphabet)
     self._construct_fail(alphabet)
-    self._construct_next(alphabet)
+    self._construct_nxt(alphabet)
 
   def _construct_goto(self, keywords, alphabet):
     for k, k_len in keywords:
@@ -37,7 +38,7 @@ class AhoCorasickAutomaton:
       current_state.update_goto(a, next_state)
       current_state = next_state
 
-    current_state.append_outputs([(keyword, keyword_len)])
+    current_state.append_outputs([keyword_len])
 
   def _construct_fail(self, alphabet):
     q = Queue()
@@ -60,36 +61,36 @@ class AhoCorasickAutomaton:
           child.update_fail(child_fallback)
           child.append_outputs(child_fallback.output())
 
-  def _construct_next(self, alphabet):
+  def _construct_nxt(self, alphabet):
     q = Queue()
     for a in alphabet:
       a_child = self._root.goto(a)
-      self._root.update_next(a, a_child)
+      self._root.update_nxt(a, a_child)
       if a_child != self._root:
         q.put(a_child)
-    self._root.use_only_next()
+    self._root.use_only_nxt()
 
     while not q.empty():
       current = q.get()
       for a, child in [(a, current.goto(a)) for a in alphabet]:
         if child is not None:
           q.put(child)
-          current.update_next(a, child)
+          current.update_nxt(a, child)
         else:
           fallback = current.fail()
-          current.update_next(a, fallback.next(a))
-      current.use_only_next()
+          current.update_nxt(a, fallback.nxt(a))
+      current.use_only_nxt()
 
   def find_occurrences(self, text, n):
     state = self._root
     for i in range(1, n + 1):
-      state = state.next(text[i])
-      for keyword, keyword_len in state.output():
-        yield i - keyword_len + 1, keyword
+      state = state.nxt(text[i])
+      for keyword_len in state.output():
+        yield i - keyword_len + 1, i + 1
 
   class Node:
     def __init__(self):
-      self._goto, self._fail, self._output, self._next = {}, None, [], {}
+      self._goto, self._fail, self._output, self._nxt = {}, None, [], {}
 
     def goto(self, a):
       return self._goto.get(a, None)
@@ -100,8 +101,8 @@ class AhoCorasickAutomaton:
     def output(self):
       return self._output
 
-    def append_outputs(self, words):
-      self._output += words
+    def append_outputs(self, outputs):
+      self._output += outputs
 
     def fail(self):
       return self._fail
@@ -109,20 +110,12 @@ class AhoCorasickAutomaton:
     def update_fail(self, target_node):
       self._fail = target_node
 
-    def next(self, a):
-      return self._next[a]
+    def nxt(self, a):
+      return self._nxt[a]
 
-    def update_next(self, a, target_node):
-      self._next[a] = target_node
+    def update_nxt(self, a, target_node):
+      self._nxt[a] = target_node
 
-    def use_only_next(self):
+    def use_only_nxt(self):
       del self._goto
       del self._fail
-
-
-if __name__ == '__main__':
-  ac = create_aho_corasick_automaton([('#he', 2), ('#she', 3),
-                                      ('#his', 3), ('#hers', 4)],
-                                     'hesru')
-  for i, p in ac.find_occurrences('#ushers', 6, ):
-    print(i, p)
