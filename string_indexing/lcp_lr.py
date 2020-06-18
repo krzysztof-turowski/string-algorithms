5df5ee075ed07febe82c5a1f231883a817c9a43c
Jan Havránek <j.havranek@gmail.com>
2020-06-18 16:28:11 +0200

    Style

--- string_indexing/lcp_lr.py
+++ string_indexing/lcp_lr.py
@@ -15,9 +15,19 @@ def lcplr_from_lcp(lcp, n):
   return lcplr
 
 
+def get_word_to_edge_lcp(l, r):
+  return l if l >= r else r
+
+
+def get_edge_to_mid_lcp(l, r, lcplr, left, mid, right):
+  return lcplr[(left, mid)] if l >= r else lcplr[(mid, right)]
+
+
 def get_word_to_mid_lcp(SA, text, word, mid, word_to_edge_lcp, edge_to_mid_lcp):
   if edge_to_mid_lcp >= word_to_edge_lcp:
-    word_to_mid_lcp = word_to_edge_lcp + get_longest_common_prefix(text[SA[mid] + word_to_edge_lcp:], word[1 + word_to_edge_lcp:])
+    word_to_mid_lcp = word_to_edge_lcp + \
+      get_longest_common_prefix(text[SA[mid] + word_to_edge_lcp:],
+                                word[1 + word_to_edge_lcp:])
   else:
     word_to_mid_lcp = edge_to_mid_lcp
   return word_to_mid_lcp
@@ -31,19 +41,20 @@ def initialize_lr(SA, text, word, n):
 
 def right_bound_with_lcplr(SA, lcplr, text, word, n, m):
   l, r = initialize_lr(SA, text, word, n)
-  
+
   if r == m or word[r + 1:] > text[SA[n] + r:]:
     return n + 1
 
   left, right = 1, n
   while left + 1 < right:
     mid = (left + right) // 2
-    if l >= r:
-      word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid, word_to_edge_lcp = l, edge_to_mid_lcp = lcplr[(left, mid)])
-    else:
-      word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid, word_to_edge_lcp = r, edge_to_mid_lcp = lcplr[(mid, right)])
-    
-    if word_to_mid_lcp == m or word[1 + word_to_mid_lcp] > text[SA[mid] + word_to_mid_lcp]:
+    word_to_edge_lcp = get_word_to_edge_lcp(l, r)
+    edge_to_mid_lcp = get_edge_to_mid_lcp(l, r, lcplr, left, mid, right)
+    word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid,
+                                          word_to_edge_lcp, edge_to_mid_lcp)
+
+    if word_to_mid_lcp == m or \
+       word[1 + word_to_mid_lcp] > text[SA[mid] + word_to_mid_lcp]:
       left, l = mid, word_to_mid_lcp
     else:
       right, r = mid, word_to_mid_lcp
@@ -52,25 +63,26 @@ def right_bound_with_lcplr(SA, lcplr, text, word, n, m):
 
 def left_bound_with_lcplr(SA, lcplr, text, word, n, m):
   l, r = initialize_lr(SA, text, word, n)
-  
+
   if l == m or word[l + 1:] <= text[SA[0] + l:]:
     return 1
   if r < m and word[r + 1:] > text[SA[n] + r:]:
     return n + 1
-  
+
   left, right = 1, n
   while left + 1 < right:
     mid = (left + right) // 2
-    if l >= r:
-      word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid, word_to_edge_lcp = l, edge_to_mid_lcp = lcplr[(left, mid)])
-    else:
-      word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid, word_to_edge_lcp = r, edge_to_mid_lcp = lcplr[(mid, right)])
-    
-    if word_to_mid_lcp == m or word[1 + word_to_mid_lcp] <= text[SA[mid] + word_to_mid_lcp]:
+    word_to_edge_lcp = get_word_to_edge_lcp(l, r)
+    edge_to_mid_lcp = get_edge_to_mid_lcp(l, r, lcplr, left, mid, right)
+    word_to_mid_lcp = get_word_to_mid_lcp(SA, text, word, mid,
+                                          word_to_edge_lcp, edge_to_mid_lcp)
+
+    if word_to_mid_lcp == m or \
+       word[1 + word_to_mid_lcp] <= text[SA[mid] + word_to_mid_lcp]:
       right, r = mid, word_to_mid_lcp
     else:
       left, l = mid, word_to_mid_lcp
-      
+
   return right
 
 
