2dcb049ead65b034911b2124d4f6c25b9a7c100f
Kamil Rajtar <Kamil@rajtar.pl>
2020-06-21 12:53:20 +0200

    Better variables names.

--- approximate_string_matching/dont_care.py
+++ approximate_string_matching/dont_care.py
@@ -3,15 +3,18 @@ import scipy.signal
 def basic_fft(text, word, n, m):
   if n < m:
     return
-  A = set(list(text[1:] + word[1:]))
-  A.discard('?')
+  alphabet = set(list(text[1:] + word[1:]))
+  alphabet.discard('?')
   mismatches = [0] * (n - m + 1)
-  for a in A:
-    text_a_mask = [int(c == a) for c in text[1:]]
-    for b in A:
-      if a != b:
-        word_b_mask = [int(c == b) for c in reversed(word[1:])]
+  for first_letter in alphabet:
+    masked_text = [int(current_char == first_letter) for current_char in
+                   text[1:]]
+    for second_letter in alphabet:
+      if first_letter != second_letter:
+        masked_word = [int(current_char == second_letter) for current_char in
+                       reversed(word[1:])]
         mismatches_ab = scipy.signal.convolve(
-            text_a_mask, word_b_mask, mode = 'valid', method = 'fft')
+            masked_text, masked_word, mode = 'valid', method = 'fft')
         mismatches = [x + y for x, y in zip(mismatches, mismatches_ab)]
-  yield from (i + 1 for i, v in enumerate(mismatches) if v == 0)
+  yield from (index + 1 for index, is_mismatch in enumerate(mismatches) if
+              is_mismatch == 0)
