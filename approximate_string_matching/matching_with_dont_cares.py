2050ff94b594992a70fb8637e4e68b9de129866f
Kamil Rajtar <Kamil@rajtar.pl>
2020-06-27 20:30:07 +0200

    Less is more.

--- approximate_string_matching/matching_with_dont_cares.py
+++ approximate_string_matching/matching_with_dont_cares.py
@@ -4,12 +4,6 @@ import scipy.signal
 def times(x, y):
   return list(starmap(lambda a, b: a * b, zip(x, y)))
 
-def times_constant(x, c):
-  return list(map(lambda a: a * c, x))
-
-def plus(x, y):
-  return list(starmap(lambda a, b: a + b, zip(x, y)))
-
 def convolve(x, y):
   return scipy.signal.convolve(x, y, mode = 'valid', method = 'fft')
 
@@ -31,37 +25,19 @@ def exact_matching_with_dont_cares(text, pattern, n, m):
   mapped_pattern = list(map(lambda x: int(x != 0), pattern))
 
   first_component = convolve(
-      times(
-          mapped_pattern,
-          times(
-              pattern,
-              pattern)
-          ),
+      times(mapped_pattern, times(pattern, pattern)),
       mapped_text
   )
-  second_component = times_constant(
-      convolve(
-          times(
-              mapped_pattern,
-              pattern
-          ),
-          times(
-              mapped_text,
-              text)
-          ),
-      -2
+  second_component = convolve(
+      times(mapped_pattern, pattern),
+      times(mapped_text, text)
   )
   third_component = convolve(
       mapped_pattern,
-      times(
-          mapped_text,
-          times(
-              text,
-              text
-          )
-      )
+      times(mapped_text,times(text,text))
   )
 
-  result = plus(plus(first_component, second_component), third_component)
+  result = [first - 2 * second + third for first, second, third in
+            zip(first_component, second_component, third_component)]
 
   yield from (index + 1 for index, value in enumerate(result) if value == 0)
