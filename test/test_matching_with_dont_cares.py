6b8da730fac996b9538cb7ec58906d94fdbbcc9c
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2020-06-30 02:08:45 +0200

    Apply random and exhaustive tests

--- test/test_matching_with_dont_cares.py
+++ test/test_matching_with_dont_cares.py
@@ -1,13 +1,17 @@
+import itertools
+import os
 import unittest
 
-from approximate_string_matching.matching_with_dont_cares \
-  import exact_matching_with_dont_cares
+from generator import rand
+from approximate_string_matching import dont_care, matching_with_dont_cares
 
-class ExactMatchingWithDontCaresCase(unittest.TestCase):
+class TestExactMatchingWithDontCaresCase(unittest.TestCase):
+  run_large = unittest.skipUnless(
+      os.environ.get('LARGE', False), 'Skip test in small runs')
+
   def make_test(self, text, pattern, result):
-    matches = exact_matching_with_dont_cares(text, pattern,
-                                             len(text),
-                                             len(pattern))
+    matches = matching_with_dont_cares.exact_matching_with_dont_cares(
+        text, pattern, len(text), len(pattern))
     self.assertEqual(result, list(matches))
 
   def test_given_input_with_no_wildcards_returns_matches(self):
@@ -18,3 +22,23 @@ class ExactMatchingWithDontCaresCase(unittest.TestCase):
 
   def test_simple(self):
     self.make_test('#aa', '#a', [1, 2])
+
+  @run_large
+  def test_random_exact_string_matching(self):
+    T, n, m, A = 100, 500, 10, ['a', 'b']
+    for _ in range(T):
+      t, w = rand.random_word(n, A), rand.random_word(m, A + ['?'])
+      reference = list(dont_care.basic_fft(t, w, n, m))
+      self.make_test(t, w, reference)
+
+  @run_large
+  def test_all_exact_string_matching(self):
+    N, M, A = 7, 3, ['a', 'b']
+    for n in range(2, N + 1):
+      for m in range(1, M + 1):
+        for t in itertools.product(A, repeat = n):
+          t = '#' + ''.join(t)
+          for w in itertools.product(A + ['?'], repeat = m):
+            w = '#' + ''.join(w)
+            reference = list(dont_care.basic_fft(t, w, n, m))
+            self.make_test(t, w, reference)
