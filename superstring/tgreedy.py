23bc5ee34ad20005f6c4fcd5ebd0f18a64fc8e64
Michal Z <michal.zwonek@gmail.com>
2020-07-01 18:00:41 +0200

    Fixed issues according to review

--- superstring/tgreedy.py
+++ superstring/tgreedy.py
@@ -1,27 +1,22 @@
+from itertools import product
+from itertools import permutations
+
 from common import prefix
 
 # computes ov(w_1, w_2)
 def overlap(w_1, w_2):
-  if w_1 != w_2:
-    w = w_2 + w_1
-  else:
-    w = w_1
+  w = w_1 if w_1 == w_2 else w_2 + w_1
   return prefix.prefix_suffix(w, len(w) - 1)[len(w) - 1]
 
 # achieves 4-approximation
 def greedy(words):
   S = set(words)
   while len(S) > 1:
-    best_ov = -1
-    chosen_pair = ('','')
-    for w_1 in S:
-      for w_2 in S:
-        if w_1 == w_2:
-          continue
-        ov = overlap(w_1, w_2)
-        if ov > best_ov:
-          best_ov = ov
-          chosen_pair = (w_1, w_2)
+    best_ov, chosen_pair = -1, None
+    for w_1, w_2 in permutations(S, 2):
+      ov = overlap(w_1, w_2)
+      if ov > best_ov:
+        best_ov, chosen_pair = ov, (w_1, w_2)
     w_1, w_2 = chosen_pair
     S.remove(w_1)
     S.remove(w_2)
@@ -30,17 +25,13 @@ def greedy(words):
 
 # first phase of mgreedy, computes the set T
 def _mgreedy_first_phase(words):
-  S = set(words)
-  T = set()
+  S, T = set(words), set()
   while S:
-    best_ov = -1
-    chosen_pair = ('','')
-    for w_1 in S:
-      for w_2 in S:
-        ov = overlap(w_1, w_2)
-        if ov > best_ov:
-          best_ov = ov
-          chosen_pair = (w_1, w_2)
+    best_ov, chosen_pair = -1, None
+    for w_1, w_2 in product(S, repeat=2):
+      ov = overlap(w_1, w_2)
+      if ov > best_ov:
+        best_ov, chosen_pair = ov, (w_1, w_2)
     w_1, w_2 = chosen_pair
     S.remove(w_1)
     if w_1 != w_2:
@@ -49,6 +40,11 @@ def _mgreedy_first_phase(words):
     else:
       T.add(w_1)
   return T
+
+# achieves 4-approximation
+def mgreedy(words):
+  return next(iter(words), [''])[0] + \
+         ''.join(w[1:] for w in _mgreedy_first_phase(words))
 
 # achieves 3-approximation
 def tgreedy(words):
