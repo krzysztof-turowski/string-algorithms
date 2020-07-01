1a75fd2b0dade829e73c07043ddcde6c220cf8b6
BartekJachowicz <bartek.jachowicz@gmail.com>
2020-07-01 12:52:30 +0200

    Fix problem with needleman_wunsch test problem and strings are 1-indexed now

--- test/test_linear_space_lcs.py
+++ test/test_linear_space_lcs.py
@@ -15,19 +15,17 @@ class TestLinearSpaceLongestCommonSubsequence(unittest.TestCase):
   """Test if substr is subsequence of the word"""
   def is_subsequence(self, word, substr):
     j = 0
-    for i in range(0, len(word)):
-      if word[i] == substr[j]:
+    for i, c in enumerate(word):
+      if c == substr[j]:
         j = j + 1
       if j == len(substr):
         return True
 
-    return False
-
   """Check if lcs length equal to returned by needleman_wunsch algorithm
       and if lcs is substring of both word given as arguments"""
   def check_linear_space_lcs(self, t_1, t_2, n_1, n_2):
-    lcs_word = lcs.needleman_wunsch(t_1, t_2, n_1 - 1, n_2 - 1)
-    exact_length = len(lcs_word[0]) + 1
+    exact_word = lcs.needleman_wunsch(t_1, t_2, n_1, n_2)[0]
+    exact_length = len(exact_word)
 
     for algorithm in LCS_ALGORITHMS:
       lcs_word, lcs_length = algorithm(t_1, t_2, n_1, n_2)
@@ -46,10 +44,10 @@ class TestLinearSpaceLongestCommonSubsequence(unittest.TestCase):
                       )
 
   def test__linear_space_lcs(self):
-    self.check_linear_space_lcs('aab', 'baa', 3, 3)
-    self.check_linear_space_lcs('COELACANTH', 'PELICAN', 10, 7)
-    self.check_linear_space_lcs('TGGCCGCGCAAAAACAGC',
-                                'TGACCGCGCAAAACAGC',
+    self.check_linear_space_lcs('#aab', '#baa', 3, 3)
+    self.check_linear_space_lcs('#COELACANTH', '#PELICAN', 10, 7)
+    self.check_linear_space_lcs('#TGGCCGCGCAAAAACAGC',
+                                '#TGACCGCGCAAAACAGC',
                                 18, 17,)
 
   @run_large
@@ -57,4 +55,4 @@ class TestLinearSpaceLongestCommonSubsequence(unittest.TestCase):
     T, n_1, n_2, A = 100, 100, 50, ['a', 'b']
     for _ in range(T):
       t_1, t_2 = rand.random_word(n_1, A), rand.random_word(n_2, A)
-      self.check_linear_space_lcs(t_1, t_2, len(t_1), len(t_2))
+      self.check_linear_space_lcs(t_1, t_2, len(t_1)-1, len(t_2)-1)
