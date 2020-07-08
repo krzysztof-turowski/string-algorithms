a65cc155b42fc86b8cad389dcbfc810b75660a67
Michal Stobierski <lampard1996@gmail.com>
2020-07-08 11:53:38 +0200

    Optimal parsing in dynamic dictionary-based data compression (e.g. LZ78, LZW) (#23)
    * LZW compression with greedy parsing
    * Optimal parsing WIP
    * LZ78 implementation
    * Refactor dict parsers
    * Add unit tests
    * Add project description

---
+++ compression/core/dictionary.py
@@ -0,0 +1,73 @@
+# pylint: disable=too-many-instance-attributes
+class TrieDict:
+  def __init__(self, label, edge=None, depth=0, index=None, parent=None):
+    self.children = {}
+    self.link, self.terminal = None, None
+
+    self.label = label
+    self.edge = edge
+    self.depth = depth
+    self.index = index
+    self.parent = self if parent is None else parent
+
+  def insert(self, w, code, index=None):
+    if not w:
+      self.terminal = True
+      self.label = code
+      self.index = index
+      return self
+    if w[0] not in self.children:
+      child = TrieDict(-1, w[0], self.depth + 1, index, self)
+      self.children[w[0]] = child
+      return child.insert(w[1:], code, index)
+
+    child = self.children[w[0]]
+    return child.insert(w[1:], code, index)
+
+  def search(self, w):
+    if not w:
+      return self
+    if w[0] not in self.children:
+      return None
+    child = self.children[w[0]]
+    return child.search(w[1:])
+
+  def extend(self, c):
+    return self.children.get(c, None)
+
+  def contract(self):
+    current = self.link.parent
+    while current.parent != current and current.link is None:
+      current = current.parent
+    return current.link
+
+  def connect(self, node):
+    self.link = node
+
+# pylint: disable=no-self-use
+class TrieReverseTrie:
+  def __init__(self):
+    self.trie = TrieDict(None, index=0)
+    self.trie_rev = TrieDict(None, index=0)
+    self.size = 0
+
+    self.trie.connect(self.trie_rev)
+    self.trie_rev.connect(self.trie)
+
+  def insert(self, node, w, wr, code):
+    self.size += 1
+    new_node = node.insert(w, code, self.size)
+    new_node_rev = self.trie_rev.insert(wr, code, self.size)
+    new_node.connect(new_node_rev)
+    new_node_rev.connect(new_node)
+    return new_node
+
+  def search(self, w):
+    return self.trie.search(w)
+
+  def get_prefix(self, node):
+    prefix = ""
+    while node.parent != node:
+      prefix += node.edge
+      node = node.parent
+    return prefix[::-1]
---
+++ compression/core/parser.py
@@ -0,0 +1,92 @@
+# pylint: disable=too-few-public-methods
+class DictParser:
+  def __init__(self, dictionary, label_extractor, extension_callback):
+    self.dict = dictionary
+    self.phrase = self.dict.trie
+    self.label_extractor = label_extractor
+    self.extension_callback = extension_callback
+
+  def parse(self, c):
+    extended = self.phrase.extend(c)
+    if extended is None:
+      reversed_phrase = str(self.dict.get_prefix(self.phrase) + c)[::-1]
+      node = self.dict.insert(
+          self.phrase, c, reversed_phrase, self.label_extractor(self, c)
+      )
+      self.phrase = self.extension_callback(self, c)
+      return node
+    self.phrase = extended
+    return None
+
+class OutputParser:
+  def __init__(self, dictionary):
+    self.dict = dictionary
+    self.phrase = self.dict.trie
+
+  def parse(self, c):
+    raise NotImplementedError
+
+  def parse_end(self):
+    raise NotImplementedError
+
+class GreedyOutputParser(OutputParser):
+  def parse(self, c):
+    extended = self.phrase.extend(c)
+    if extended is None:
+      code = self.phrase.label if self.phrase.label is not None else None
+      self.phrase = self.dict.search(c)
+      return code
+    self.phrase = extended
+    return None
+
+  def parse_end(self):
+    code = self.parse('')
+    return [code] if code is not None else []
+
+class OptimalOutputParser(OutputParser):
+  def __init__(self, dictionary):
+    super(OptimalOutputParser, self).__init__(dictionary)
+    self.beginning, self.f_beginning, self.offset = 1, 1, 0
+    self.tmp_out = ""
+
+  def parse(self, c):
+    self.tmp_out += c
+    extended = self.phrase.extend(c)
+    if extended is None:
+      tmp_offset, tmp_depth = 0, self.phrase.depth
+      while True:
+        longest_suffix = self.phrase.contract()
+        tmp_offset += self.phrase.depth - longest_suffix.depth
+        extended = longest_suffix.extend(c)
+        if extended is None:
+          self.phrase = longest_suffix
+        else:
+          self.phrase = extended
+          break
+
+      if self.beginning + self.offset + tmp_offset > self.f_beginning + 1:
+        tmp_node = self.dict.search(self.tmp_out[0:self.offset])
+        code = tmp_node.label
+        self.tmp_out = self.tmp_out[self.offset:]
+
+        self.beginning += self.offset
+        self.f_beginning = self.beginning + tmp_depth - 1
+        self.offset = tmp_offset
+        return code
+
+      self.offset += tmp_offset
+      return None
+
+    self.phrase = extended
+    return None
+
+  def parse_end(self):
+    code = []
+    if self.offset > 0:
+      tmp_node = self.dict.search(self.tmp_out[0:self.offset])
+      code.append(tmp_node.label)
+      self.tmp_out = self.tmp_out[self.offset:]
+    if self.tmp_out:
+      tmp_node = self.dict.search(self.tmp_out)
+      code.append(tmp_node.label)
+    return code
