f2c0275b48c9a44d746560c50ff596ee5d238928
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2020-10-31 07:15:24 +0100

    First part of the lecture notes: exact string matching

--- text/code/other/prefix-suffix.py
+++
@@ -1,8 +0,0 @@
-def prefix_suffix(w, m):
-  B, t = [-1] + [0] * m, -1
-  for i in range(1, m + 1):
-    while t >= 0 and w[t + 1] != w[i]:
-      t = B[t] # prefikso-sufiks to relacja przechodnia
-    t = t + 1
-    B[i] = t
-  return B
--- text/code/suffix-array/from-suffix-tree.py
+++
@@ -1,9 +0,0 @@
-def suffix_array_from_suffix_tree(text, n):
-  def traverse(v, depth):
-    depth -= len(v.label)
-    if len(v.children) == 0:
-      return [depth]
-    return [d for _, child in sorted(v.children.items())
-            for d in traverse(child, depth)]
-  ST, _ = suffix_tree.mccreight(text, n)
-  return traverse(ST, n + 2)
