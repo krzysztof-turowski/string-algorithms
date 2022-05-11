31d3f09f9a9e311a1eb825c39b478bb1f81210f2
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2022-05-11 08:19:31 +0200

    Remove duplicated tests

--- test/test_common.py
+++ test/test_common.py
@@ -15,23 +15,3 @@ class TestCommon(unittest.TestCase):
     self.assertEqual(prefix.get_overlap('#aa','#aa'), 1)
     self.assertEqual(prefix.get_overlap('#aab','#aab'), 0)
     self.assertEqual(prefix.get_overlap('#undergrounder', '#undergrounder'), 5)
-
-  @run_large
-  def test_all_weak_boyer_moore_shift(self):
-    M, A = 10, ['a', 'b', 'c']
-    for m in range(2, M + 1):
-      for w in itertools.product(A, repeat = m):
-        w = '#' + ''.join(w)
-        reference = suffix.weak_boyer_moore_shift_brute_force(w, m)
-        result = suffix.weak_boyer_moore_shift(w, m)
-        self.assertEqual(result, reference)
-
-  @run_large
-  def test_all_boyer_moore_shift(self):
-    M, A = 10, ['a', 'b', 'c']
-    for m in range(2, M + 1):
-      for w in itertools.product(A, repeat = m):
-        w = '#' + ''.join(w)
-        reference = suffix.boyer_moore_shift_brute_force(w, m)
-        result = suffix.boyer_moore_shift(w, m)
-        self.assertEqual(result, reference)
