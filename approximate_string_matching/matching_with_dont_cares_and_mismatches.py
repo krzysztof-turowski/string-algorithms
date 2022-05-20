7d670bf3297fb8ff0faa8354a57433b7e14e222a
Grzegorz Gawryał <ggawryal@gmail.com>
2022-05-20 11:27:23 +0200

    Strongly selective families

--- approximate_string_matching/matching_with_dont_cares_and_mismatches.py
+++ approximate_string_matching/matching_with_dont_cares_and_mismatches.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import signal
+from scipy.stats import binom
+import itertools
 
 def __get_clifford_array(text_mapped, word_mapped, positional):
   indices = np.ones(len(text_mapped)) if not positional else np.arange(len(text_mapped))
@@ -106,3 +108,74 @@ def __checking_stage(text_mapped, word_mapped, mismatches):
   ]
   return [i for i,x in enumerate(number_of_mismatches) if x != 'too many mismatches']
 
+
+def entropy(x, q):
+  if x in [0,1]:
+    return 0
+  return x*np.log((q-1)/x)/np.log(q) + (1-x)*np.log(1/(1-x)/np.log(q))
+
+def is_prime(n):
+  if n <= 1:
+    return False
+  i = 2
+  while i*i <= n:
+    if n%i == 0:
+      return False
+    i += 1
+  return True
+
+def generate_ssf(n, r):
+  if r*r*np.log(n) >= n:
+    return [{i} for i in range(n)]
+  
+  delta = (r-1)/r
+  q = 2*r
+  while not is_prime(q):
+    q+=1
+
+  k = int(np.ceil(np.log(n) / np.log(q)))
+  m = int(np.ceil(k/(1-entropy(delta,q))))
+  matrix = derandomized(m, k, delta, q)
+  S = {(i,l): set() for i in range(m) for l in range(q)}
+
+  for i,y in enumerate(itertools.product(range(q), repeat=k)):
+    z = (matrix@y)%q
+    for p in range(m):
+      if i < n:
+        S[(p, z[p])].add(i)
+
+  return list(x for x in S.values() if len(x) > 0)
+
+def derandomized(m, k, delta, q):
+  G = np.zeros((m,k), dtype=int)
+  modular_inverse = all_modular_inverses_array(q)
+
+  for i in range(m):
+    for j in range(k):
+      W = np.zeros(q, dtype=np.float64)
+      for x in range(1,q):
+        for x_r in itertools.product(range(q), repeat=j):
+          y = np.array(list(x_r)+[x]+[0]*(k-j-1))
+          v = (-modular_inverse[x])*sum(G[i][t]*x_r[t] for t in range(j)) % q
+          Gy = G@y
+          c = sum(Gy[t]%q != 0 for t in range(i))
+          W[v] -= binom.pmf(np.floor(delta*m-c), m-j, 1-1/q)
+      G[i][j] = np.argmax(W)
+  return G
+
+def all_modular_inverses_array(modulo):
+  modular_inverse = np.zeros(modulo, dtype=int)
+  modular_inverse[1] = 1
+  for i in range(2, modulo):
+    modular_inverse[i] = (modulo-(modulo//i)*modular_inverse[modulo%i])%modulo
+  return modular_inverse
+
+def verify_if_is_strongly_selective(ss, n, r):
+  for A in itertools.combinations(range(n), r):
+    for a in A:
+      sel = False
+      for B in ss:
+        sel |= set(A).intersection(B) == {a}
+        if sel:
+          break
+      assert sel
