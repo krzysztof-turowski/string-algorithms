750d8a0830c706e484147010ad336467c1f44d22
krzysztofpioro <krzysztof.pioro1998@gmail.com>
2022-06-24 00:35:47 +0200

    fixes after review

--- lyndon/lyndon_factorization.py
+++ lyndon/lyndon_factorization.py
@@ -5,12 +5,8 @@ def duval(text, n, less = operator.__lt__):
   while i <= n:
     j, k = i + 1, i
     while j <= n and not less(text[j], text[k]):
-      if less(text[k], text[j]):
-        k = i
-      else:
-        k += 1
+      k = i if less(text[k], text[j]) else k + 1
       j += 1
     while i <= k:
       yield (i, i + j - k)
       i += j - k
-  
