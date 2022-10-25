28927cdeb04ec1218d1a126903e48e2c93108198
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2022-10-25 21:41:22 +0200

    Add Adamczyk-Rytter maximum suffix and Duval Lyndon factorization algorithms

---
+++ lyndon/lyndon_factorization.py
@@ -0,0 +1,12 @@
+import operator
+
+def duval(text, n, less = operator.__lt__):
+  i = 1
+  while i <= n:
+    j, k = i + 1, i
+    while j <= n and not less(text[j], text[k]):
+      k = i if less(text[k], text[j]) else k + 1
+      j += 1
+    while i <= k:
+      yield (i, i + j - k)
+      i += j - k
