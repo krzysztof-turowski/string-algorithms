ebe739ec437f7d5c2f4e723eb1567dce959273e8
julianles <julian.lesniak@student.uj.edu.pl>
2023-07-03 23:16:17 +0200

    Improved Apostolico-Giancarlo implementation

--- exact_string_matching/backward.py
+++ exact_string_matching/backward.py
@@ -119,8 +119,9 @@ def turbo_boyer_moore(t, w, n, m):
 
 
 def boyer_moore_apostolico_giancarlo(t, w, n, m):
+  max_suf = [max(i,0) for i in suffix.maximum_suffixes(w[:-1], m-1)]
   def check_ends(i, k):
-    return w[max(0, i - k) + 1:i] == w[m - min(k, i) + 1:m]
+    return max_suf[i-1] >= min(k, i) - 1
   BM = suffix.boyer_moore_shift(w, m)
   skip = [0] * (n + 1)
   i = 1
@@ -136,6 +137,28 @@ def boyer_moore_apostolico_giancarlo(t, w, n, m):
       j = 1
     skip[i + m - 1] = m - j
     i = i + BM[j]
+
+def boyer_moore_apostolico_giancarlo_bad_shift(t, w, n, m):
+  max_suf = [max(i,0) for i in suffix.maximum_suffixes(w[:-1], m-1)]
+  def check_ends(i, k):
+    return max_suf[i-1] >= min(k, i) - 1
+  BM = suffix.boyer_moore_shift(w, m)
+  LAST = suffix.last_occurrence(w[:-1])
+  skip = [0] * (n + 1)
+  i = 1
+  while i <= n - m + 1:
+    j = m
+    while j > 0:
+      if check_ends(j, skip[i + j - 1]) and t[i + j - 1] == w[j]:
+        j = j - max(1, skip[i + j - 1])
+      else:
+        break
+    if j <= 0:
+      yield i
+      j = 1
+    skip[i + m - 1] = m - j
+    bad_character = LAST.get(t[i + j - 1], 0)
+    i = i + max(BM[j], j - bad_character)
 
 def bitap_shift_add(text: str, word: str, _n: int, m: int):
   A = set(text[1:]) | set(word[1:])
