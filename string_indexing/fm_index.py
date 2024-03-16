72fb4755a0a2bf9fdc86aa4a26491ba182d66c87
prolik123 <mattti2015@gmail.com>
2024-03-16 09:45:56 +0100

    Dodano unit testy

--- string_indexing/fm_index.py
+++ string_indexing/fm_index.py
@@ -1,24 +1,10 @@
-from suffix_array import naive
-#from compression import burrows_wheeler
-#from compression.burrows_wheeler import transform_from_suffix_array
-#from compression.burrows_wheeler import inverse_transform_naive
-def transform_from_suffix_array(SA, text, n):
-  return '#' + ''.join(
-      text[SA[i] - 1] if SA[i] > 1 else '$' for i in range(n + 1))
-
-def inverse_transform_naive(BWT, n):
-  reversal = [''] * (n + 1)
-  for _ in range(n + 1):
-    reversal = [c + r for (r, c) in zip(sorted(reversal), BWT[1:])]
-  return '#' + ''.join(sorted(reversal)[0][1:])
-
 
 class FMIndex:
    
    # all of strings beginns with # (idk why?)
    # i sppose that patterns do not starts with #
    
-   def __init__ (self, SA, BWT, n):
+   def __init__ (self, SA, BWT, text, n):
       self.L = BWT
       self.F = '#$' + ''.join(text[SA[i]] for i in range(1, n + 1))
       self.n = n
@@ -43,7 +29,7 @@ class FMIndex:
       currentSample = 0
       self.closestSample = [0]
       for i in range(1, n+2):
-         if abs(currentSample-i) > abs(currentSample + self.sampleSize-i) and i + self.sampleSize < self.n:
+         if abs(currentSample-i) > abs(currentSample + self.sampleSize-i) and (i + self.sampleSize < self.n):
             currentSample += self.sampleSize
          self.closestSample.append(currentSample)
 
@@ -57,11 +43,9 @@ class FMIndex:
                currValue += 1
             if i == nextSample:
                self.occInSampleForChar[c].append(currValue)
-      #print(self.occInSampleForChar)
-      #print(self.closestSample)
-      #print(self.begginings)
-      #print(self.mapperOfChar)
+               nextSample = nextSample + self.sampleSize
 
+    # should be private
    def getRangeOfOccurence(self, p, size):
       if size > self.n:
         return [-1, -1]
@@ -69,7 +53,7 @@ class FMIndex:
       currChar = p[size-1]
       if currChar not in self.mapperOfChar:
          return [-1, -1]
-      
+
       mapIdx = self.mapperOfChar[currChar]
       l = self.begginings[mapIdx]
       r = self.n + 1
@@ -77,13 +61,11 @@ class FMIndex:
          r = self.begginings[mapIdx + 1] - 1
       
       for i in range(size-2, -1, -1):
-         #print(l, r)
          currChar = p[i]
          if currChar not in self.mapperOfChar:
             return [-1, -1]
          occurencesBefore = self._getOcc(currChar, l - 1)
          occurencesAfter = self._getOcc(currChar, r)
-         #print('OCC ', occurencesBefore, occurencesAfter)
          if occurencesBefore == occurencesAfter:
             return [-1, -1]
          mapIdx = self.mapperOfChar[currChar]
@@ -91,9 +73,9 @@ class FMIndex:
          r = self.begginings[mapIdx] + occurencesAfter - 1
          if r < l:
             return [-1, -1]
-      #print(l, r)
       return [l, r]
 
+    # O(|p|)
    def count(self, p, size):
       ran = self.getRangeOfOccurence(p, size)
       if ran[0] == -1:
@@ -116,29 +98,21 @@ class FMIndex:
          
       return self.occInSampleForChar[c][(closestSample)//self.sampleSize] + toAdd
       
-
+    #O(|p|)
    def query(self, p, l):
       return self.count(p, l) > 0
    
+   # O(|p| + k) where k is the number or occurances of p in text
    def get_all_occurrance(self, p, l):
       arr = self.getRangeOfOccurence(p, l)
       if arr[0] == -1:
          return []
-      return [self.SA[i-1] for i in range(arr[0], arr[1] + 1)]      
+      return [self.SA[i-1] for i in range(arr[0], arr[1] + 1)]
 
-
-text = '#ababa'
-n = 5
-SA = naive(text, n)
-BWT = transform_from_suffix_array(SA, text, n)
-print(text)
-print(SA)
-index = FMIndex(SA, BWT, n)
-print(index.get_all_occurrance('aa', 2))
-print(index.count('aab', 3))
-print(index.count('a', 1))
-print(index.count('b', 1))
-print(index.get_all_occurrance('aba', 3))
-print(index.count('c', 1))
-print(index.count('caab', 4))
-print(index.count('abaaba', 6))
+    # O(|p|)
+   def get_any_occurrance(self, p, l):
+      arr = self.getRangeOfOccurence(p, l)
+      if arr[0] == -1:
+         return -1
+      return self.SA[arr[0]-1]
+   
